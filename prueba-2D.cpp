79c1e6ca577c4cf2fe0f80022acd74562554a2ee
sarinconp <sarinconp@unal.edu.co>
2021-06-11 16:24:03 -0500

    cambios a aux_position

--- prueba-2D.cpp
+++ prueba-2D.cpp
@@ -9,7 +9,7 @@ int random_step();//función que determina el avance del caminante en cada paso
 int main(int argc, char **argv)
 {
   int dimension=2; //dimensiones a utilizar
-  int total_steps=10; //cantidad de pasos a llevar a cabo, en un futuro se podría implementar un while y que se desarrolle hasta que el caminante quede encerrado, además de que aún no se ha implementado un "marco" dónde encerrar al caminante
+  int total_steps=20; //cantidad de pasos a llevar a cabo, en un futuro se podría implementar un while y que se desarrolle hasta que el caminante quede encerrado, además de que aún no se ha implementado un "marco" dónde encerrar al caminante
   int history [total_steps][dimension]={};//este es el array que guarda los puntos para que no se repitan
   int aux_position [dimension]={};//este array es un auxiliar que almacena el nuevo paso antes de enviarlo a position, la motivación detrás de esto se explicará más adelante
   int position [dimension]={};//este array es la posicion del caminante en un momento dado
@@ -34,15 +34,15 @@ int main(int argc, char **argv)
 	    }
 	}
       std::cout<<"proof"<<"\t"<<proof<<std::endl;//esto cout se puso con proposito de visualizar que el sistema funcionaba
-      if(proof==1)//si proof=1 (es decir, se dió una posición repetida), se vuelve a inicializar proof=0, en el historial se vuelve a escribir la posición previa (el array position) y aux_position toma los valores del paso anterior(array position). Esto es para que el camino sea consistente y no hayan pasos dónde se avance más rapido.
+      if(proof==1)//si proof=1 (es decir, se dió una posición repetida)
 	{
-	  proof=0;
+	  proof=0;//se inicializa proof=0 
 	  
-	  history[ii+1][0] = position[0];
-	  history[ii+1][1] = position[1];
+	  history[ii+1][0] = aux_position[0];//se escribe en el historial la posición repetida (no es necesario para que funcione pero ayuda a evidenciar cuando hay una posición repetida y saber que el codigo funciona
+	  history[ii+1][1] = aux_position[1];
 	  // std::cout<<"aux position"<<"\t"<<aux_position[0]<<"\t"<<aux_position[1]<<std::endl;
 	  //std::cout<<"position"<<"\t"<<position[0]<<"\t"<<position[1]<<std::endl;
-	  aux_position[0]=position[0];
+	  aux_position[0]=position[0];//se remplaza aux_position por la posición del paso anterior (position) para poder empezar el ciclo otra vez y que el caminante pueda moverse a una casilla que no sea repetida.
 	  aux_position[1]=position[1];
 	  continue;
 	}
