dc9de507f7949cd4fd14f7c6959a8daf3a759689
Ronalito <rscortessa@unal.edu.co>
2021-06-17 19:25:54 -0500

    Se modulariza el código

---
+++ funciones.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <cmath>
+int* random_step(int dimension, int *r);
+double final_position(int dimension, int total_steps);
+double average(int dimension, int total_steps, int walks);
