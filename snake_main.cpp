7bc0e300356a7b1bf306b1034c664ed9cc8929d7
jpradas1 <jpradas@unal.edu.co>
2021-06-27 12:40:48 -0500

    primeras modificaciones para realizar ñas métricas

--- snake_main.cpp
+++ snake_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "mpi.h"
 #include "snake.h"
 
@@ -29,7 +30,9 @@ int main (int argc, char** argv)
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   jungle snakes(P, snake(N));
-   double muertes=0;
+  double muertes=0;
+
+  double t0 = MPI_Wtime();
   for(int i=0;i<t;i++)
   {
    
@@ -39,11 +42,22 @@ int main (int argc, char** argv)
       }
   }
   std::vector<double> lifetime=print_promedios(t,snakes,"snake"+std::to_string(N)+".txt",pid,np,P);
+  double t1 = MPI_Wtime();
+  double time1 = t1-t0;
 
   if(pid==0)
     {
       std::cout<<N<<"\t"<<lifetime[0]<<"\t"<<lifetime[1]<<std::endl;
     }
+  double t2 = MPI_Wtime();
+  double time2 = t2-t0;
+
+  if(pid == 0){
+    std::cout << np << "\t" << time1 << "\t" << time2 << std::endl;
+  }
+
+
+
   MPI_Finalize();
   return 0;
 }
