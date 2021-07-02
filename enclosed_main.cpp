9c6f5fd1c8eed2814ef6005f3a203b10eae2fffc
JoseBernalRodriguez <live@slackware.example.org>
2021-07-01 20:19:27 -0500

    update

--- enclosed_main.cpp
+++ enclosed_main.cpp
@@ -29,13 +29,14 @@ int main (int argc, char** argv)
       } // each snake is stepped until it dies
   }
 
-  std::vector<std::vector<int>> graves(PP); //Death possition of our snakes
-  for(int ii=0; ii < PP ; ii++)
-  {graves.push_back(snakes[ii].r);} //as all snakes are dead, r is our death possition
+  if(N==2)
+  {
+  std::vector<std::vector<int>> grid((2*Square+1)*2); //GRid that represents ponts on the the square
 
-  
-  
-  MPI_Finalize();
-    
+  // grid(0) is the left upper corner //grid[2*(Square+1)*N-1] is the lower right corner
+  //grid[Square*(2*Square+1)+Square-1] Represents the origin
+ 
+  }
+   MPI_Finalize();
 }
 
